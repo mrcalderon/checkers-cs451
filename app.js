96020121e662ae7bad16e64213639dd1caa74b49
Ryan Efendy <ryanefendy95@gmail.com>
2017-03-05 15:09:44 -0500

    delete unnecessary libraries

--- app.js
+++ app.js
@@ -3,10 +3,10 @@ var app = express();
 app.use(express.static('public'));
 var http = require('http').Server(app);
 var io = require('socket.io')(http); // setup socket server
-var port = process.env.PORT || 3001;
+var port = process.env.PORT || 3000;
 
 app.get('/', function(req, res) {
-    res.sendFile(__dirname + '/public/default.html');
+    res.sendFile(__dirname + '/public/index.html');
 });
 
 http.listen(port, function() {
@@ -18,7 +18,11 @@ io.on('connection', function(socket) {
     console.log('new connection');
 
     // Called when the client calls socket.emit('move')
-    socket.on('move', function(msg) {
-        socket.broadcast.emit('move', msg);
+    socket.on('move', function(move) {
+        socket.broadcast.emit('move', move);
+    });
+
+    socket.on('remove', function(data) {
+        socket.broadcast.emit('remove', data);
     });
 });
